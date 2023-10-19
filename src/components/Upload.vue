56a57745bc638b4f2729396597e87b9566343a22
Dionis Gonzalez <dionisggr@gmail.com>
2023-10-18 23:49:38 -0600

    responsive Upload

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="container mx-auto py-16">
-    <div class="text-center mb-10">
-      <h1 class="text-4xl font-semibold">Upload New Art</h1>
-      <p class="text-gray-700 mt-2">
+  <div class="container mx-auto px-4 sm:px-6 py-12 sm:py-16" style="min-height: calc(100vh - 365px)">
+    <div class="text-center mb-8 sm:mb-10">
+      <h1 class="text-3xl sm:text-3xl lg:text-4xl font-semibold">Upload New Art</h1>
+      <p class="text-gray-700 mt-2 px-4">
         Upload multiple artworks at once and provide their details
       </p>
     </div>
 
     <div>
-      <!-- The input is now hidden using the 'hidden' class -->
       <input
         ref="fileInput"
         type="file"
@@ -16,13 +15,12 @@
         @change="previewFiles"
         class="hidden"
       />
-      <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-10">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 mt-6 sm:mt-10">
         <div
           v-for="(image, index) in images"
           :key="index"
-          class="relative group rounded-xl p-4 transition duration-300 ease-in-out hover:shadow-lg"
+          class="relative group rounded-xl p-2 m-4 sm:p-4 transition duration-300 ease-in-out hover:shadow-lg"
         >
-          <!-- Delete button -->
           <button
             @click="triggerRemove(index)"
             class="absolute top-2 right-2 bg-red-500 text-white text-xs p-1 px-2 rounded-full transition duration-300 ease-in-out transform hover:bg-red-600 opacity-0 group-hover:opacity-100"
@@ -30,17 +28,13 @@
             <i class="fas fa-times"></i>
           </button>
 
-          <!-- Flex container -->
-          <div class="flex items-center space-x-4">
-            <!-- Image -->
+          <div class="flex flex-col sm:flex-row items-start sm:items-center space-y-4 sm:space-y-0 sm:space-x-4">
             <img
               :src="image.src"
               alt="Preview"
-              class="w-1/4 rounded-lg shadow-md transition duration-300 ease-in-out transform group-hover:scale-105"
+              class="w-full sm:w-1/4 rounded-lg shadow-md transition duration-300 ease-in-out transform group-hover:scale-105"
             />
-
-            <!-- Input fields -->
-            <div>
+            <div class="w-full">
               <input
                 v-model="image.title"
                 placeholder="Title"
@@ -57,32 +51,28 @@
         </div>
       </div>
 
-      <div class="mt-8 text-center relative">
-        <!-- "Upload Art" Button -->
+      <div class="mt-6 sm:mt-8 text-center relative">
         <button
           @click="openUploadWizard"
-          class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 mb-4"
+          class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-5 sm:px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 mb-4"
         >
           Upload Art
         </button>
         <br />
-
-        <!-- "Publish" Button -->
         <button
           @click="publish"
           @mouseenter="showTooltip = !images.length"
           @mouseleave="showTooltip = false"
-          class="bg-green-500 hover:bg-green-600 text-white py-2 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+          class="bg-green-500 hover:bg-green-600 text-white py-2 px-5 sm:px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
           :class="{ 'opacity-50 cursor-not-allowed': !images.length }"
           :disabled="!images.length"
         >
           Publish
         </button>
 
-        <!-- Tooltip for the "Publish" button -->
         <span
           v-if="showTooltip"
-          class="absolute transform translate-y-12 p-2 bg-gray-600 text-white text-xs rounded shadow-lg"
+          class="absolute transform -translate-y-16 -translate-x-1/2 p-2 bg-gray-600 text-white text-xs rounded shadow-lg"
           style="left: 50%"
         >
           Upload an image first!
@@ -91,16 +81,12 @@
     </div>
   </div>
 
-  <!-- Confirmation Modal with Blurred Background -->
   <div
     v-if="showModal"
     class="fixed inset-0 flex items-center justify-center z-50"
   >
-    <!-- Blurred Background Overlay -->
     <div class="absolute inset-0 bg-black opacity-50 backdrop-blur-md"></div>
-
-    <!-- Modal Content -->
-    <div class="bg-white p-4 rounded shadow-lg w-1/2 z-10">
+    <div class="bg-white p-4 rounded shadow-lg w-11/12 md:w-1/2 z-10">
       <h3 class="text-xl mb-4">Confirm Deletion</h3>
       <p>Are you sure you want to remove this image?</p>
       <div class="mt-4 flex justify-end space-x-2">
