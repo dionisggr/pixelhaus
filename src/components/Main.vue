274f0c29cd48e972ac877e9781fb4e8bcb09cb98
Dionis Gonzalez <dionisggr@gmail.com>
2023-11-17 01:35:43 -0700

    mvp ;

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -811,15 +811,15 @@
             </a>
             <a
               href="#"
-              class="text-blue-700 hover:text-blue-800 text-5xl transition duration-300 ease-in-out transform hover:scale-125"
-            >
-              <i class="fab fa-linkedin"></i>
-            </a>
-            <a
-              href="#"
               class="text-red-500 hover:text-red-600 text-5xl transition duration-300 ease-in-out transform hover:scale-125"
             >
               <i class="fab fa-pinterest-square"></i>
+            </a>
+            <a
+              href="#"
+              class="text-blue-700 hover:text-blue-800 text-5xl transition duration-300 ease-in-out transform hover:scale-125"
+            >
+              <i class="fab fa-tiktok"></i>
             </a>
           </div>
         </div>
@@ -927,6 +927,46 @@
         </div>
       </div>
     </div>
+
+<!-- Welcome Modal -->
+<div
+    class="fixed inset-0 bg-black bg-opacity-70 overflow-y-auto h-full w-full flex justify-center items-center pb-20"
+    id="welcomeModal"
+    v-if="!disableWelcomeModal"
+  >
+    <!-- Modal Content -->
+    <div class="relative w-1/2 mx-auto border border-gray-200 bg-white rounded-lg shadow-xl">
+      <div class="bg-gray-50 px-6 py-4 border-b border-gray-200 rounded-t-lg">
+        <h3 class="text-2xl font-semibold text-gray-800">
+          <i class="fas fa-palette mr-2"></i>Welcome to <span class="italic">PixelHaus</span>
+        </h3>
+      </div>
+      <div class="p-6">
+        <p class="text-md text-gray-700">
+          <span class="font-semibold">Discover</span> a world where <span class="italic">art meets technology</span>. <span class="italic">PixelHaus</span> brings you an exquisite collection of <span class="font-semibold">AI-generated art</span>, crafted into stunning wall art pieces for your space. Explore, rent, and enjoy a revolving gallery at your fingertips.
+        </p>
+        <p class="text-md text-gray-700 mt-4">
+          <span class="font-bold">Note:</span> <span class="italic">PixelHaus</span> is a <span class="italic">conceptual app</span>, still preparing for commercial launch. <span class="font-bold">Stay tuned for a canvas of possibilities!</span>
+        </p>
+        <div class="flex justify-between items-center mt-6">
+          <label class="flex items-center">
+            <input
+              type="checkbox"
+              class="form-checkbox h-5 w-5 text-blue-600"
+              v-model="disableWelcomeModal"
+            />
+            <span class="ml-2 text-gray-700 text-sm">Don't show again</span>
+          </label>
+          <button
+            class="px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow"
+            @click="disableWelcomeModal = true"
+          >
+            Explore
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 
     <!-- Footer -->
     <footer class="bg-white text-gray-700 mt-4 border-t border-gray-300">
@@ -1182,6 +1222,7 @@ export default {
       openModal: null,
       notifications: [],
       cart: [],
+      disableWelcomeModal: localStorage.getItem('hidePixelHausModal'),
       sidebar: {
         Popular: { icon: 'star', color: 'blue' },
         New: { icon: 'new_releases', color: 'green' },
@@ -1435,6 +1476,13 @@ export default {
         );
       } else {
         this.displayedArts = this.arts.slice(0, this.itemsToShow);
+      }
+    },
+    disableWelcomeModal(newVal) {
+      if (newVal) {
+        localStorage.setItem('hidePixelHausModal', true);
+      } else {
+        localStorage.removeItem('hidePixelHausModal');
       }
     },
   },
